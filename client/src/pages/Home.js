ad7c1ca2b2e93046c03b330c2f40c5c96143c5a8
Mikaela <M.Norrelokke@hotmail.com>
2021-05-20 15:23:46 +0200

    some movie changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,25 @@
 import { MovieContext } from "../contexts/MovieContext";
 import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+
+
+
 
 function Home() {
+  const history = useHistory();
+  const handleClick = (movieId) => {
+ 
+    if (movieId === undefined) {
+      history.push(`/`);
+    } else {
+      history.push(`/movies/${movieId}`);
+    }
+  };
   const { movies } = useContext(MovieContext);
 
   const movieitems = movies.map((movie) => (
     <div key={movie._id} 
+    onClick={() => handleClick(movie._id)}
     className="movie">
       <img src={movie.poster} alt={movie.title} />
       <div className="movie-info">
