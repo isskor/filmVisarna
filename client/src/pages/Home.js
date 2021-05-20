0dfcffbce858ef89e60c9d37ab442bde7988cb09
August <augustraro112@hotmail.com>
2021-05-20 13:48:12 +0200

    Added a bootstrap carousel and some styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,9 @@
+import MovieBannerImage from "../components/MovieBannerImage";
+
 function Home() {
   return (
     <div>
-      <h1 primary>This is home</h1>
-      <div className='row'>
-        <div className='col-lg-6'>hello</div>
-        <div className='col-lg-6'>hello2</div>
-      </div>
+      <MovieBannerImage />
     </div>
   );
 }
