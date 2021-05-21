e4b9d66a5d477cdc8f5087548d4bbc1db58d79e3
jerry <jphuong91@gmail.com>
2021-05-21 11:51:18 +0200

    fix conflicts

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,6 +1,6 @@
-import { MovieContext } from "../contexts/MovieContext";
-import { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { MovieContext } from '../contexts/MovieContext';
+import { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function DetailPage() {
   const { movie, getMovieById } = useContext(MovieContext);
@@ -15,7 +15,7 @@ function DetailPage() {
     <div>
       {movie && (
         <>
-          <div className="trailer">
+          <div className='trailer'>
             <iframe
               src={
                 movie.trailer +
@@ -23,45 +23,45 @@ function DetailPage() {
                 '?controls="0"' +
                 '?rel="0"'
               }
-              allowfullscreen="true"
+              allowfullscreen='true'
             ></iframe>
           </div>
-          <div className="info">
-            <div className="cover">
+          <div className='info'>
+            <div className='cover'>
               <img src={movie.poster} />
             </div>
-            <div className="mainInfo">
-              <div className="title"> {movie.title} </div>
+            <div className='mainInfo'>
+              <div className='title'> {movie.title} </div>
 
-              <div className="grayText"> Directors </div>
-              <div className="director"> {movie.director}</div>
+              <div className='grayText'> Directors </div>
+              <div className='director'> {movie.director}</div>
 
-              <div className="grayText"> Cast </div>
-              <div className="whiteText">{movie.actors}</div>
+              <div className='grayText'> Cast </div>
+              <div className='whiteText'>{movie.actors}</div>
 
-              <div className="grayText"> Plot </div>
-              <div className="whiteText">{movie.plot}</div>
+              <div className='grayText'> Plot </div>
+              <div className='whiteText'>{movie.plot}</div>
             </div>
-            <div className="sideInfo">
-              <div className="grayText"> Release Date </div>
-              <div className="whiteText"></div>
+            <div className='sideInfo'>
+              <div className='grayText'> Release Date </div>
+              <div className='whiteText'></div>
 
-              <div className="grayText"> Genres </div>
-              <div className="genres">{movie.genres}</div>
+              <div className='grayText'> Genres </div>
+              <div className='genres'>{movie.genres}</div>
 
-              <div className="grayText"> Age Rating </div>
-              <div className="whiteText">{movie.rated}</div>
+              <div className='grayText'> Age Rating </div>
+              <div className='whiteText'>{movie.rated}</div>
 
-              <div className="grayText"> Length </div>
-              <div className="whiteText">{movie.runTime} min </div>
+              <div className='grayText'> Length </div>
+              <div className='whiteText'>{movie.runTime} min </div>
 
-              <div className="grayText"> Language </div>
-              <div className="whiteText">{movie.language}</div>
+              <div className='grayText'> Language </div>
+              <div className='whiteText'>{movie.language}</div>
             </div>
           </div>
-          <div className="booking">
-            <div className="showTimes"> Show times </div>
-          </div>{" "}
+          <div className='booking'>
+            <div className='showTimes'> Show times </div>
+          </div>{' '}
         </>
       )}
     </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,13 @@
-import { useState } from "react";
-import FilterModal from "../components/FilterModal";
-import { MovieContext } from "../contexts/MovieContext";
-import { useContext } from "react";
-import { useHistory } from "react-router-dom";
+import { useState } from 'react';
+import FilterModal from '../components/FilterModal';
+import { MovieContext } from '../contexts/MovieContext';
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 function Home() {
   const [openFilter, setOpenFilter] = useState(false);
 
-
-
-
-function Home() {
   const history = useHistory();
   const handleClick = (movieId) => {
- 
     if (movieId === undefined) {
       history.push(`/`);
     } else {
@@ -22,26 +17,29 @@ function Home() {
   const { movies } = useContext(MovieContext);
 
   const movieitems = movies.map((movie) => (
-    <div key={movie._id} 
-    onClick={() => handleClick(movie._id)}
-    className="movie">
+    <div
+      key={movie._id}
+      onClick={() => handleClick(movie._id)}
+      className='movie'
+    >
       <img src={movie.poster} alt={movie.title} />
-      <div className="movie-info">
+      <div className='movie-info'>
         <h3>{movie.title}</h3>
       </div>
     </div>
   ));
 
   return (
-    <div className="Home">
+    <div className='Home'>
       <FilterModal open={openFilter} setOpen={setOpenFilter} />
       <button onClick={() => setOpenFilter(true)}>filter</button>
-      <div className="movie-container">{movieitems}</div>
-      <div className="row">
-        <div className="col-lg-6">hello</div>
-        <div className="col-lg-6">hello2</div>
+      <div className='movie-container'>{movieitems}</div>
+      <div className='row'>
+        <div className='col-lg-6'>hello</div>
+        <div className='col-lg-6'>hello2</div>
       </div>
     </div>
   );
 }
+
 export default Home;
