3c1e91aae3a9a9c01648867d68b703902ef318b9
Mikaela <M.Norrelokke@hotmail.com>
2021-05-21 09:31:23 +0200

    bugg

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,7 +10,7 @@ export default function Login() {
         <Container >
             <h1 className="text-center login-info">Login</h1>
             <Form>
-            <Alert variant={"danger"} className={`${styles.Alert} ${isError ? styles.Alert.active : styles.Alert.inactive}`}>You did not enter the correct credentials</Alert>
+            {/* <Alert variant={"danger"} className={`${styles.Alert} ${isError ? styles.Alert.active : styles.Alert.inactive}`}>You did not enter the correct credentials</Alert> */}
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label className="login-info">Email</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" required />
