8a674557811ec521ec58b0c84a28eb70baa33b41
Mikaela <M.Norrelokke@hotmail.com>
2021-05-24 23:02:20 +0200

    calendar changes

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,18 +1,24 @@
 import { MovieContext } from '../contexts/MovieContext';
 import { useContext, useEffect,useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Calendar from 'react-calendar';
+import BookingCalendar from 'react-booking-calendar';
 
 function DetailPage() {
   const { movie, getMovieById } = useContext(MovieContext);
   const history = useParams();
   console.log(history);
-  const [date, setDate] = useState(new Date(2021,2,25));
+  const bookings = [
+    new Date(2016, 7, 1),
+    new Date(2016, 7, 2),
+    new Date(2016, 7, 3),
+    new Date(2016, 7, 9),
+    new Date(2016, 7, 10),
+    new Date(2016, 7, 11),
+    new Date(2016, 7, 12),
+  ];
 
-  const onChange = date => {
-    setDate(date);
-    console.log(date);
-  }
+
+
   useEffect(() => {
     getMovieById(history.id);
   }, []);
@@ -67,20 +73,11 @@ function DetailPage() {
           </div>
           <div className='booking'>
             <div className='showTimes'> Show times </div>
-            <div className="Sample">
   
-      <div className="Sample__container">
-        <main className="Sample__container__content">
-          <Calendar
-            onChange={onChange}
-            value={date}
-          />
-        </main>
-      </div>
-    </div>
-          </div>{date.toString()}
+          </div>
         </>
       )}
+          <BookingCalendar bookings={bookings} />
     </div>
   );
 }
