42070ccbeccdcee0d8fd05ffe556685c0874cc66
August <augustraro112@hotmail.com>
2021-05-25 10:52:05 +0200

    Quick fix

--- client/src/pages/RegisterThxPage.js
+++ client/src/pages/RegisterThxPage.js
@@ -4,13 +4,13 @@ import { useHistory } from "react-router-dom";
 const RegisterThxPage = () => {
   const history = useHistory();
   const goToHome = () => {
-    history.push("/");
+    history.push("/login");
   };
 
   return (
     <div className="registerThxPage">
       <h1>Thanks for registering!</h1>
-      <button onClick={goToHome}>Back to home</button>
+      <button onClick={goToHome}>Log in</button>
     </div>
   );
 };
