31005bcd8251da7ebbb96189e9583df7e74b86d5
Mikaela <M.Norrelokke@hotmail.com>
2021-05-31 22:00:25 +0200

    calendar update

--- client/src/components/BookingCalendar.js
+++ client/src/components/BookingCalendar.js
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { useState, useContext, useCallback } from 'react';
 import Calendar from 'react-calendar';
+import { MovieContext } from '../contexts/MovieContext';
+import ShowTimes from '../components/ShowTimes';
 
 function BookingCalendar() {
   const [date, SetDate] = useState(new Date());
-   
+  const { fetchShowtimeByDate } = useContext(MovieContext);
+  const [showTimes, setShowTimes] = useState([]);
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  
 const onChange = (date) => {
   SetDate(date);
+  fetchShow(date);
+  console.log(showTimes)
 }
+
+const fetchShow = useCallback(
+  async (date) => {
+    console.log(date);
+    const shows = await fetchShowtimeByDate(date.toLocaleDateString(undefined, options));
+    console.log(shows);
+    setShowTimes(shows);
+  },
+  [fetchShowtimeByDate]
+);
 
   return (
     <div>
@@ -15,6 +32,8 @@ const onChange = (date) => {
       value={date}
     />
     <h1>{date.toString()}</h1>
+    <div className='showTimes row'> Show times </div>
+            <ShowTimes showTimes={showTimes} />
   </div>
   );
 }
