760b05dbc3f7ca113343ff0493d6912f27f8f01c
August <augustraro112@hotmail.com>
2021-05-31 14:38:14 +0200

    Added some styling to UpcomingBookings

--- client/src/pages/UpcomingBookings.js
+++ client/src/pages/UpcomingBookings.js
@@ -1,19 +1,23 @@
 import React, { useEffect, useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import TicketCard from "../components/TicketCard";
+import { useHistory } from "react-router-dom";
 
 const UpcomingBookings = () => {
   const { loggedInUser } = useContext(UserContext);
-
+  const history = useHistory();
   return (
-    <div>
+    <div className="bookingsWrapper">
       <h1>Upcoming bookings</h1>
       {loggedInUser && loggedInUser.bookings.length > 0 ? (
         loggedInUser.bookings.map((booking, index) => (
           <TicketCard key={index} booking={booking} />
         ))
       ) : (
-        <h3>You have no upcoming bookings...</h3>
+        <>
+          <h3>You have no upcoming bookings...</h3>
+          <button onClick={() => history.push("/")}>Go to home</button>
+        </>
       )}
     </div>
   );
