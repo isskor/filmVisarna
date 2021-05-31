d0b6ec0cd034cafc3c56a1fcf5d558ac6e7d9867
Leo <leoeriksson01@gmail.com>
2021-05-31 13:17:52 +0200

    Finished footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,38 @@
-import { Container } from "react-bootstrap";
-import Login from "../components/Login";
-import Register from "../components/Register";
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="footerContainer">
       <div className="footerMain">
         <div className="footerSection">
+          <Link to="/About" className="footerLink">
+            {" "}
+            About Us{" "}
+          </Link>
+          <p className="footerLink"> Help </p>
           <p className="footerLink"> Contact Us </p>
+          <p className="footerLink"> Information </p>
+          <p className="footerLink"> Join our team </p>
         </div>
-        <div className="footerSection">Hej2</div>
-        <div className="footerSection">Hej3</div>
+        <div className="footerSection">
+          <p className="footerLink"> Cookie Policy </p>
+          <p className="footerLink"> Privacy </p>
+          <p className="footerLink"> Legal </p>
+        </div>
+        <div className="footerSection">
+          <p className="footerLink"> Android Download </p>
+          <p className="footerLink"> iOS Download </p>
+        </div>
       </div>
-      <div className="footerBottom"></div>
+      <div className="footerBottom">
+        <div className="footerSocials">
+          <div className="twitter"> </div>
+          <div className="youtube"> </div>
+          <div className="instagram"> </div>
+          <div className="facebook"> </div>
+        </div>
+        <p> © Filmvisarna 2015 to 2021. All rights reserved. </p>
+      </div>
     </div>
   );
 }
