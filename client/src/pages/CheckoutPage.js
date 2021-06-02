74de5266a7c8b07b318266f1eb1a3e5ef45dd08a
Torbjörn <torbjorn.lindfors@gmail.com>
2021-06-02 11:48:17 +0200

    Added some stuff

--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import {CartContext} from '../contexts/CartContext';
+import { CartContext } from '../contexts/CartContext';
 
 const CheckoutPage = () => {
   const { cartBookings } = useContext(CartContext);
@@ -7,11 +7,25 @@ const CheckoutPage = () => {
   return (
     <div className='container checkoutpage'>
       <h1>Checkout</h1>
-      <p>Insert TicketCard component here</p>
-      {cartBookings?.map((booking) => {
-        return <p>seats{booking.seatRows}</p>;
-      })}
-      <p>Tickets x8</p>
+      
+      <div className="checkoutTicketCard">
+        {cartBookings?.map((booking) => {
+          return (<div className="tickets">
+            <div className="checkoutPoster">
+              <img src={booking.showtime.movie.poster} alt="" />
+            </div>
+            <div className="checkoutInfo">
+              <h3>{booking.showtime.movie.title}</h3>
+              <p>seats{booking.seatRows}</p>
+              {/* <p>Tickets{booking.tickets}</p> */}
+            </div>
+            <div className="checkoutInfoTotal">
+              <h2>Total 520</h2>
+            </div>
+          </div>);
+        })};
+      </div>
+
       <div className='checkoutTotal'>
         <h2>Total: 520</h2>
         <div className='button'>
