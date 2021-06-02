e884cf8f201855d069ca3ced6f793219d475d914
jerry <jphuong91@gmail.com>
2021-06-02 10:13:59 +0200

    fix conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -13,51 +13,52 @@ import RegisterThxPage from './pages/RegisterThxPage';
 import ProfilePage from './pages/ProfilePage';
 import BookingSeatPage from './pages/BookingSeatPage';
 import BookingCalendar from './components/BookingCalendar.js';
-import CartContextProvider from "./contexts/CartContext";
-import Footer from "./components/Footer";
-
+import CartContextProvider from './contexts/CartContext';
+import Footer from './components/Footer';
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <MovieContextProvider>
           <FilterContextProvider>
             <UserContextProvider>
               <CartContextProvider>
                 <Nav />
-                <Route exact path="/">
+                <Route exact path='/'>
                   <Home />
                 </Route>
 
-                <Route exact path="/about">
+                <Route exact path='/about'>
                   <About />
                 </Route>
-                <Route exact path="/loginpage">
+                <Route exact path='/loginpage'>
                   <LoginPage />
                 </Route>
-                <Route exact path="/login">
+                <Route exact path='/login'>
                   <Login />
                 </Route>
-                <Route exact path="/movies/:id">
+                <Route exact path='/movies/:id'>
                   <DetailPage />
                 </Route>
-                <Route exact path="/showtime/:id">
+                <Route exact path='/showtime/:id'>
                   <BookingSeatPage />
                 </Route>
-                <Route exact path="/thank-you-for-registering">
+                <Route exact path='/thank-you-for-registering'>
                   <RegisterThxPage />
                 </Route>
-                <Route exact path="/Profile">
+                <Route exact path='/Profile'>
                   <ProfilePage />
                 </Route>
                 <Route exact path='/checkout'>
                   <CheckoutPage />
                 </Route>
+                <Route exact path='/calendar'>
+                  <BookingCalendar />
+                </Route>
+                <Footer />
               </CartContextProvider>
-              <Route exact path='/calendar'>
-                <BookingCalendar />
-              </Route>
             </UserContextProvider>
           </FilterContextProvider>
         </MovieContextProvider>
