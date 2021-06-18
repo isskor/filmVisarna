3edf2947499cc7c66b04de51ce7ba047c64b1809
August <augustraro112@hotmail.com>
2021-06-18 11:30:56 +0200

    Changed redirect time from 5 seconds to 30 seconds

--- client/src/pages/RedirectPage.js
+++ client/src/pages/RedirectPage.js
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
 export default function RedirectPage({ userRoute }) {
-  const [timer, setTimer] = useState(5);
+  const [timer, setTimer] = useState(30);
   const history = useHistory();
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (timer === 0) {
-        history.push('/');
+        history.push("/");
       }
       setTimer(timer - 1);
     }, 1000);
@@ -15,27 +15,27 @@ export default function RedirectPage({ userRoute }) {
   });
 
   return (
-    <div className='container my-5'>
-      <div className='row '>
-        <h2 className='text-center'>
+    <div className="container my-5">
+      <div className="row ">
+        <h2 className="text-center">
           {userRoute
-            ? ' You Must Be logged in to Proceed to this page'
-            : 'Page Not Found'}
+            ? " You Must Be logged in to Proceed to this page"
+            : "Page Not Found"}
         </h2>
       </div>
-      <div className='row'>
-        <div className='col text-center my-5'>
+      <div className="row">
+        <div className="col text-center my-5">
           {userRoute && (
-            <Link to='/loginpage'>
-              <button className='my-3'>Login</button>
+            <Link to="/loginpage">
+              <button className="my-3">Login</button>
             </Link>
           )}
-          <Link to='/'>
+          <Link to="/">
             <button>Back to Home</button>
           </Link>
         </div>
       </div>
-      <h2 className='text-center'>redirecting to home in {timer}</h2>
+      <h2 className="text-center">redirecting to home in {timer}</h2>
     </div>
   );
 }
